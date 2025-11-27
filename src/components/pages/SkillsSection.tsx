3e2694a09f7a072ae61c07579ac57de378af9454
KukuhAgung <anculapuak07@gmail.com>
2025-11-27 18:01:24 +0700

    feat: add portfolio section [1]

---
+++ src/components/pages/SkillsSection.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+
+export default function Skills() {
+    return (
+      <Fragment>
+        <section className="skills">test</section>
+        <h2> My Techstack</h2>
+      </Fragment>
+    );
+}
